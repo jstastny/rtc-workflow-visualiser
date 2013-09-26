de07d9cc34a093ae47d3d5f356000f8d51188d29
Jan Stastny <honza@stastnarodina.com>
2013-09-27 00:16:41 +0200

    Initial source code import

---
+++ com/stastnarodina/workflowVisualiser/Action.java
@@ -0,0 +1,133 @@
+package com.stastnarodina.workflowVisualiser;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Action {
+	private String id;
+	private String name;
+	private State targetState;
+	
+	private Set<Resolution> resolutions = new HashSet<Resolution>();
+	
+	
+	public Action() {
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the targetState
+	 */
+	public State getTargetState() {
+		return targetState;
+	}
+
+
+	/**
+	 * @param targetState the targetState to set
+	 */
+	public void setTargetState(State targetState) {
+		this.targetState = targetState;
+	}
+
+
+	/**
+	 * @return the resolutions
+	 */
+	public Set<Resolution> getResolutions() {
+		return resolutions;
+	}
+
+
+	/**
+	 * @param resolutions the resolutions to set
+	 */
+	public void setResolutions(Set<Resolution> resolutions) {
+		this.resolutions = resolutions;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Action [id=" + id + ", name=" + name + "]";
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Action)) {
+			return false;
+		}
+		Action other = (Action) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ com/stastnarodina/workflowVisualiser/Group.java
@@ -0,0 +1,34 @@
+package com.stastnarodina.workflowVisualiser;
+
+public enum Group {
+	
+	IN_PROGRESS("inprogress"),
+	OPEN("open"),
+	CLOSED("closed"),
+	NONE("none");
+	
+	private String name;
+	
+	private Group(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public static Group getByName(String name) {
+		if("inprogress".equals(name)) {
+			return Group.IN_PROGRESS;
+		} else if("open".equals(name)) {
+			return Group.OPEN;
+		} else if("closed".equals(name)) {
+			return Group.CLOSED;
+		}
+		return Group.NONE;
+			
+	}
+	
+	
+	
+}
